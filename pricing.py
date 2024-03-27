4c0d1d6aed46e87c5de715893e78df073e75170c
codyp626 <62960554+codyp626@users.noreply.github.com>
2024-03-27 15:58:09 -0500

    changes

--- pricing.py
+++ pricing.py
@@ -5,12 +5,12 @@ DEBUG = False
 
 
 def getDivPrice():
-    return 155.0
+    return 290
 
 # gets item hashes from poe api (corrupted items not yet suported)
 
 
-def getItemHashes(search_name=None, search_basetype=None, implicits=None, itemlvl=None, quality=None, corrupted="false"):
+def getItemHashes(search_name=None, search_basetype=None, implicits=None, itemlvl=None, quality=None, corrupted=None):
     url = "https://www.pathofexile.com/api/trade/search/Standard"
 
     payload = {
@@ -23,12 +23,24 @@ def getItemHashes(search_name=None, search_basetype=None, implicits=None, itemlv
          {
             "misc_filters":
             {
+                "disabled": False,
                 "filters":
                 {
-                    "quality": {"min": quality},
-                    "corrupted": {"option": corrupted},
-                    "ilvl": {"min": itemlvl}
+                    # "quality": {"min": quality},
+                    # "corrupted": {"option": None},
+                    # "ilvl": {"min": itemlvl}
                 }
+            },
+            "trade_filters":
+            {
+                "disabled": False,
+                    "filters":
+                        {
+                            "price":
+                                {
+                                    "option":"chaos_divine"
+                                }
+                        }
             }
         }
         },
@@ -42,17 +54,14 @@ def getItemHashes(search_name=None, search_basetype=None, implicits=None, itemlv
     if search_basetype is not None:
         payload["query"]["type"] = search_basetype
     if implicits is not None:
-        payload["query"]["stats"]["filters"]["id"] = {
-            "id": "pseudo.pseudo_number_of_implicit_mods", "value": {"min": implicits}}
-
-    # print(payload["query"])
+        payload["query"]["stats"] = [{"type":"and","filters":[{"id":"pseudo.pseudo_number_of_implicit_mods","value":{"min":implicits},"disabled":False}]}]
+    if corrupted is not None:
+        payload["query"]["filters"]["misc_filters"]["filters"]["corrupted"]["option"] = corrupted
+        
+    # set corrupted or not
     payload = json.dumps(payload)
-
-    # print(payload)
+    print(payload)
     response = requests.request("POST", url, headers=init_stuff.getHeaders(), data=payload)
-    # if response != "<Response [200]>":
-    #     print(response.text)
-    #     return None
     resp_obj = response.json()
     if 'error' in response.text:
         print(resp_obj)
@@ -106,12 +115,12 @@ def returnPriceAvg(results):
             break
         curr_string = result['listing']['price']['currency']
         amount = result['listing']['price']['amount']
-        print("price:", amount, curr_string, sep='\t')
+        # print("price:", amount, curr_string, sep='\t')
         if curr_string == "divine":
-            sum += amount
+            sum += amount*getDivPrice()
             sum_divisor += 1
         elif curr_string == "chaos":
-            sum += amount/getDivPrice()
+            sum += amount
             sum_divisor += 1
         # else:
             # print("error got bad currency type")
@@ -122,7 +131,7 @@ def returnPriceAvg(results):
     return "%0.1f" % ((sum*1.0)/sum_divisor)
 
 
-def PriceItem(search_name=None, search_basetype=None, implicits=None, itemlvl=None, quality=None, corrupted="false"):
+def PriceItem(search_name = None, search_basetype = None, implicits = None, itemlvl = None, quality = None, corrupted = None):
     # name is for unique names ex: search_name = "Mageblood", search_basetype = "Heavy Belt"
     # currency is a base type
 
@@ -137,13 +146,11 @@ def PriceItem(search_name=None, search_basetype=None, implicits=None, itemlvl=No
     return returnPriceAvg(results)
     # printResults(results)
 
-# def topFivePrint(results):
-#     for result in results
-#         print(result[])
-
 
 if __name__ == "__main__":
-    # print("Mirror =", PriceItem("","Mirror of Kalandra"))
+    print("Mirror =", PriceItem(search_basetype="Mirror of Kalandra"))
     # print("Mirror house =", PriceItem("","House of Mirrors"))
-    print("AVG PRICE =", PriceItem(search_basetype="The Apothecary"))
+    # print("AVG PRICE =", PriceItem(search_basetype="The Apothecary"))
     # print("Headhunter =", PriceItem("Headhunter","Leather Belt"))
+    # print(PriceItem(search_name="Voices", corrupted=True))
+    # print(PriceItem(search_name="Mageblood", corrupted=True, implicits=2))
--- read_from_csv.py
+++ read_from_csv.py
@@ -16,14 +16,18 @@ ILVL = 11
 PRIMORDIAL = 12
 
 
-with open('cards.csv', 'r') as file:
+with open('cards_unique.csv', 'r') as file:
     reader = csv.reader(file)
     with open('card_price.csv', mode='w') as outfile:
         for card in reader:
+            result_price = pricing.PriceItem(search_name=card[RESULT], corrupted=CORRUPTED, itemlvl=ILVL)
+            print(card[RESULT], "\t\t=\t\t", result_price)
+            time.sleep(10)
             card_price = pricing.PriceItem(search_basetype=card[NAME])
             print(card[NAME], "\t\t=\t\t", card_price)
-            writer = csv.writer(outfile)
-            row = [card[NAME], card_price]
-            writer.writerow(row)
             time.sleep(10)
+            writer = csv.writer(outfile)
+            row = [card[NAME], card[STACK_SIZE], card_price, card[RESULT], result_price]
+            writer.writerow(row)
+            
         
