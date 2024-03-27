d23132fd55d8645fc6d62d6d1972aad1db5e0c60
codyp626 <62960554+codyp626@users.noreply.github.com>
2024-03-27 11:20:53 -0500

    added second cs with some price values

--- pricing.py
+++ pricing.py
@@ -3,50 +3,50 @@ import json
 import init_stuff
 DEBUG = False
 
+
 def getDivPrice():
     return 155.0
 
-#gets item hashes from poe api (corrupted items not yet suported)
-def getItemHashes(search_name = None, search_basetype = None, implicits = 2, itemlvl = 0, quality = 0, corrupted = "false"):
-    url = "https://www.pathofexile.com/api/trade/search/Affliction"
-    
+# gets item hashes from poe api (corrupted items not yet suported)
+
+
+def getItemHashes(search_name=None, search_basetype=None, implicits=None, itemlvl=None, quality=None, corrupted="false"):
+    url = "https://www.pathofexile.com/api/trade/search/Standard"
+
     payload = {
-            "query":
-                {"status":{"option":"online"},
-                    # "name":search_name,
-                    # "type":search_basetype,
-                    "stats":[
-                        {"type":"and","filters":[
-                            {"id":"pseudo.pseudo_number_of_implicit_mods","value":{"min":implicits}}
-                            ]
-                         }
-                        ],
-                    "filters":
-                        {
-                        "misc_filters":
-                            {
-                                "filters":
-                                    {
-                                        "quality":{"min":quality},
-                                        "corrupted":{"option":corrupted},
-                                        "ilvl":{"min":itemlvl}
-                                        }
-                                    }
-                            }
-                        },
-                "sort":{"price":"asc"}
+        "query":
+        {"status": {"option": "online"},
+         # "name":search_name,
+         # "type":search_basetype,
+         "stats": [],
+         "filters":
+         {
+            "misc_filters":
+            {
+                "filters":
+                {
+                    "quality": {"min": quality},
+                    "corrupted": {"option": corrupted},
+                    "ilvl": {"min": itemlvl}
                 }
-    
-    
-    
+            }
+        }
+        },
+        "sort": {"price": "asc"}
+    }
+
+    # payload = json.dumps({"query": {"status": {"option": "online"}, "name": "Mageblood","type": "Heavy Belt", "stats": [{"type": "and", "filters": []}]}, "sort": {"price": "asc"}})
+
     if search_name is not None:
         payload["query"]["name"] = search_name
     if search_basetype is not None:
         payload["query"]["type"] = search_basetype
-        
-    print(payload["query"][""])
+    if implicits is not None:
+        payload["query"]["stats"]["filters"]["id"] = {
+            "id": "pseudo.pseudo_number_of_implicit_mods", "value": {"min": implicits}}
+
+    # print(payload["query"])
     payload = json.dumps(payload)
-     
 
     # print(payload)
     response = requests.request("POST", url, headers=init_stuff.getHeaders(), data=payload)
@@ -70,30 +70,34 @@ def getItemHashes(search_name = None, search_basetype = None, implicits = 2, ite
         print("Complexity:", resp_obj['complexity'])
         print("Results:", resp_obj['total'])
         print(result_strings)
-        
+
     return result_strings
 
 # getting results from result hashes
+
+
 def getItemResults(hashes):
     url = "https://www.pathofexile.com/api/trade/fetch/"
     if hashes is None:
         print("Error: got no hashes")
         return False
-    for i in range(10 if (len(hashes)>10) else len(hashes)):
+    for i in range(10 if (len(hashes) > 10) else len(hashes)):
         url += hashes[i] + ','
-    url = url[:-1] #remove trailing comma
+    url = url[:-1]  # remove trailing comma
     payload = {}
-    response = requests.request("GET", url, headers=init_stuff.getHeaders(), data=payload)
+    response = requests.request(
+        "GET", url, headers=init_stuff.getHeaders(), data=payload)
     resp_obj = response.json()
     item_results = resp_obj['result']
-    
+
     if DEBUG:
         print(response)
-        print (response.text)
+        print(response.text)
     return item_results
 
+
 def returnPriceAvg(results):
-    max_count = 5
+    max_count = 4
     count = 0
     sum = 0
     sum_divisor = 0
@@ -102,27 +106,27 @@ def returnPriceAvg(results):
             break
         curr_string = result['listing']['price']['currency']
         amount = result['listing']['price']['amount']
-        print("price:", amount, curr_string)
+        print("price:", amount, curr_string, sep='\t')
         if curr_string == "divine":
             sum += amount
             sum_divisor += 1
         elif curr_string == "chaos":
             sum += amount/getDivPrice()
             sum_divisor += 1
-        else:
-            print("error got bad currency type")
-            sum_divisor = 1
+        # else:
+            # print("error got bad currency type")
         if DEBUG:
-            print("price:", amount, curr_string)
+            print("price:", amount, curr_string, sep='\t')
         count += 1
-            
+
     return "%0.1f" % ((sum*1.0)/sum_divisor)
 
-def PriceItem(search_name, search_basetype):
-    #name is for unique names ex: search_name = "Mageblood", search_basetype = "Heavy Belt"
-    #currency is a base type
-    
-    hashes = getItemHashes(search_name, search_basetype)
+
+def PriceItem(search_name=None, search_basetype=None, implicits=None, itemlvl=None, quality=None, corrupted="false"):
+    # name is for unique names ex: search_name = "Mageblood", search_basetype = "Heavy Belt"
+    # currency is a base type
+
+    hashes = getItemHashes(search_name, search_basetype, implicits, itemlvl, quality, corrupted)
     if not hashes:
         print("hash FAIL")
         return
@@ -132,13 +136,14 @@ def PriceItem(search_name, search_basetype):
         return
     return returnPriceAvg(results)
     # printResults(results)
-    
+
 # def topFivePrint(results):
 #     for result in results
 #         print(result[])
 
+
 if __name__ == "__main__":
     # print("Mirror =", PriceItem("","Mirror of Kalandra"))
     # print("Mirror house =", PriceItem("","House of Mirrors"))
-    print("Mageblood =", PriceItem("Mageblood","Heavy Belt"))
+    print("AVG PRICE =", PriceItem(search_basetype="The Apothecary"))
     # print("Headhunter =", PriceItem("Headhunter","Leather Belt"))
